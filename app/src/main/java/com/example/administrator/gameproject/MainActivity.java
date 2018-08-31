c726c35f76a460a8437678321bd60899ec935eab
amatya18 <sonuamatya2@gmail.com>
2018-08-31 13:37:49 +1200

    Commit on svn

---
+++ app/src/main/java/com/example/administrator/gameproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.gameproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void open(View v) {
+
+        ImageView s1= (ImageView) findViewById(R.id.imageid1);
+      //  v.setOnClickListener("A");
+    }
+}
